86640dfdd949b82987647f2e55e514004e18427e
Jeff Woltjen <jwoltjen@us.yusen-logistics.com>
2022-08-03 15:15:53 -0500

    Style page and add api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  weatherApiBaseUrl: 'https://visual-crossing-weather.p.rapidapi.com/forecast',
+  XRapidAPIKeyHeaderName: 'X-RapidApi-Key', 
+  XRapidAPIKeyHeaderValue: 'https://visual-crossing-weather.p.rapidapi.com/forecast',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'visual-crossing-weather.p.rapidapi.com',
 };
 
 /*
